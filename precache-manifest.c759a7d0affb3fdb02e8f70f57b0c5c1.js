f064d0a35e6d39766f28dc2d4f94d98c06dce8c3
jbarget <dear.justen@gmail.com>
2020-07-12 15:47:20 +0100

    Updates

---
+++ precache-manifest.c759a7d0affb3fdb02e8f70f57b0c5c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e5ead09b41e947647e340348912749",
+    "url": "/phantom-dwellings/index.html"
+  },
+  {
+    "revision": "25e0569b4a2ab344cfa6",
+    "url": "/phantom-dwellings/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "3431326f8fd3b7cb3f54",
+    "url": "/phantom-dwellings/static/css/main.a9ac5b2e.chunk.css"
+  },
+  {
+    "revision": "25e0569b4a2ab344cfa6",
+    "url": "/phantom-dwellings/static/js/2.1b8a4383.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phantom-dwellings/static/js/2.1b8a4383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3431326f8fd3b7cb3f54",
+    "url": "/phantom-dwellings/static/js/main.96e9bdc9.chunk.js"
+  },
+  {
+    "revision": "40104b5b4ed3b79a2049",
+    "url": "/phantom-dwellings/static/js/runtime-main.f643d45f.js"
+  }
+]);
